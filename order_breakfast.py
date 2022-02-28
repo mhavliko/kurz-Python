41e324b7057d5a24f5fa2c47a1aad31ffbb9bba2
Marketa Havlikova <makihaf@seznam.cz>
2022-02-28 18:18:05 +0100

    uprava kodu

--- order_breakfast.py
+++ order_breakfast.py
@@ -13,20 +13,17 @@ if sys.argv[1] == "help":
     print (f'{drink[0]} - {drink[1]}/{int(int(drink[1])*1.5)} Kc')
   exit()
 else:
-  noPeople = int(sys.argv[3])
-  tip = sys.argv[4]
-  togo = sys.argv[5]
-
-  drinkSize = sys.argv[2].split("-")[1]
+  noPeople, tip, toGo = sys.argv[3:]
+  drinkSize = sys.argv[2].split("-")
 
   prizeFood = [dessert[1] for dessert in desserts if dessert[0] == sys.argv[1]]
-  prizeDrink = [drink[1] for drink in drinks if drink[0] == sys.argv[2].split("-")[0]]
+  prizeDrink = [drink[1] for drink in drinks if drink[0] == drinkSize[0]]
   prizeFood = int(prizeFood[0])
   prizeDrink = int(prizeDrink[0])
-  if drinkSize == "big":
+  if drinkSize[1] == "big":
     prizeDrink = int(prizeDrink * 1.5)
 
-  cena = (noPeople * (prizeFood + int(togo) * 10 + prizeDrink))
+  cena = (int(noPeople) * (prizeFood + int(toGo) * 10 + prizeDrink))
   cena += int(tip) * 0.1 * cena
   
   print(f"Objednali jste si {noPeople}x {sys.argv[1]} a {sys.argv[2]}.")
